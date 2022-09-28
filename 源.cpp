c55cd5c17c486b8c681ca837a956ef8639a987ae
HANGEplues <111443878+HANGEplues@users.noreply.github.com>
2022-09-28 09:45:23 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,25 +1,28 @@
 #include<stdio.h>
-int calculate(int Height1, int Height2);
 int main()
 {
-	int m_Height1;
-	int m_Height2;
-	int result;
-
-	printf("请输入父亲的身高（cm）\n");
-	scanf_s("%d", &m_Height1);
-
-	printf("请输入母亲的身高（cm）\n");
-	scanf_s("%d", &m_Height2);
-
-	result = calculate(m_Height1, m_Height2);
-	printf("孩子的身高是：\n");
-	printf("%d", result);
-	printf("cm");
+	int telephone, num;
+	printf("请输入您要拨打的电话：");
+	scanf_s("%d", &telephone);
+	if (telephone == 10086)
+	{
+		printf("欢迎致电中国移动，客服将为您提供以下服务：查询话费请拨1，人工服务请拨0\n");
+		scanf_s("%d", &num);
+		switch (num)
+		{
+		case 1:
+			printf("您的话费余额为100.2元\n");
+			break;
+		case 0:
+			printf("您好，欢迎使用移动客服业务，为了保证通话质量，您的通话将会被录音，谢谢合作。\n");
+			break;
+		default:
+			printf("对不起，系统没有此业务。\n");
+		}
+	}
+	else
+	{
+		printf("暂时无法提供服务。\n");
+	}
 	return 0;
-}
-int calculate(int Height1, int Height2)
-{
-	int result = (Height1 + Height2) * 0.54;
-	return result;
 }
